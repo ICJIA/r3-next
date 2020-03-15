2c9c6bed2e7a802430ab15dccc3325065a6b58ee
cschweda <cschweda@gmail.com>
2020-03-15 09:23:29 -0500

    fix: step styling

--- src/components/NofoStep.vue
+++ src/components/NofoStep.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid full-width style="border-top: 2px solid #ddd">
+  <v-container fluid full-width style="border-top: 1px solid #ccc">
     <v-row no-gutters style="min-height: 300px;">
       <v-col
         cols="12"
