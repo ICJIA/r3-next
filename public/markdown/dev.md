0f09c086f87af295ff75db8209cacf3f6542a104
cschweda <cschweda@gmail.com>
2020-05-12 15:45:59 -0500

    fix: add dev map

---
+++ public/markdown/dev.md
@@ -0,0 +1,15 @@
+---
+title: "Test map"
+showToc: false
+showInSitemap: false
+showInFooter: false
+showInNav: false
+showInSidebar: false
+summary: "Test map"
+showAsCard: false
+hideTitleOnPage: true
+---
+
+<MapTest></MapTest>
+
+Table here
---
+++ src/components/MapTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="my-10">
+    <!-- <div class="iframe-container"> -->
+    <iframe
+      width="100%"
+      height="740"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      src="http://illinois.maps.arcgis.com/apps/webappviewer/index.html?id=137fa70c604e49ad976a6e3248d80068"
+    ></iframe>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.iframe-container {
+  overflow: hidden;
+  padding-top: 56.25%;
+  position: relative;
+  border: 1px solid #ccc;
+}
+.iframe-container iframe {
+  border: 0;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+/* 4x3 Aspect Ratio */
+.iframe-container-4x3 {
+  padding-top: 75%;
+}
+</style>
